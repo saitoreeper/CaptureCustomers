439c643bfa440778f5501aca45884d0074661bc5
Dominic Joubert <saitoreeper@gmail.com>
2018-05-31 12:38:30 +0200

    Database Tables Created in Entities project

---
+++ CaptureClients.Entities/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptureClients.Entities
+{
+    public class Contact
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string ContactNumber { get; set; }
+
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
---
+++ CaptureClients.Entities/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptureClients.Entities
+{
+    public class Customer
+    {
+        [Key]
+        public int ID { get; set; }
+        public int MyProperty { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Latitude { get; set; }
+        public string longitude { get; set; }
+
+        public ICollection<Contact> Contacts { get; set; }
+    }
+}
