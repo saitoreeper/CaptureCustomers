87a7c105f4986db07bb7e5d0aa0404d6691dbf3d
Dominic Joubert <saitoreeper@gmail.com>
2018-05-31 13:48:44 +0200

    Customer Backend code completed

---
+++ CaptureClients/Controllers/CustomersController.cs
@@ -0,0 +1,53 @@
+﻿using CaptureClients.DataContexts;
+using CaptureClients.Entities;
+using CaptureClients.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CaptureClients.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<Customer> Get()
+        {
+            CustomersHelper Customer = new CustomersHelper();
+            IEnumerable<Customer> customersList = Customer.GetCustomersList();
+
+            return customersList;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public HttpResponseMessage Post([FromBody]Customer customer)
+        {
+            HttpResponseMessage request;
+            if (ModelState.IsValid)
+            {
+                CustomersHelper CustomerHelper = new CustomersHelper();
+                CustomerHelper.SaveCustomer(customer);
+                return this.Request.CreateResponse(HttpStatusCode.OK);
+            }
+            return this.Request.CreateResponse(HttpStatusCode.BadRequest);
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ CaptureClients/Helpers/CustomersHelper.cs
@@ -0,0 +1,36 @@
+﻿using CaptureClients.DataContexts;
+using CaptureClients.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace CaptureClients.Helpers
+{
+   
+
+    public class CustomersHelper
+    {
+        private CustomerCaptureDb db = new CustomerCaptureDb();
+
+        public IEnumerable<Customer> GetCustomersList()
+        {
+            List<Customer> customersList = db.Customers.ToList();
+            return customersList;
+        }
+
+        public void SaveCustomer(Customer customer)
+        {            
+                Customer addCustomer = new Customer()
+                {
+                    Name = customer.Name,
+                    Latitude = customer.Latitude,
+                    longitude = customer.longitude
+                };
+
+                db.Customers.Add(addCustomer);
+                db.SaveChanges();
+            }
+        }
+    }
